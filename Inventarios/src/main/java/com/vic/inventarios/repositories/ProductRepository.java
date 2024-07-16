201d6b67d89f1ea64dd9469a76c4e3033b127ff0
Vic <117196219+VicVaporu01@users.noreply.github.com>
2024-07-16 15:01:50 -0500

    Delete Inventarios directory

--- Inventarios/src/main/java/com/vic/inventarios/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vic.inventarios.repositories;
-
-import com.vic.inventarios.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
